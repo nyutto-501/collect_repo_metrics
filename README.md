c094a6cb622850b3a2390ecdcda23867d8b6bc80
y-funayama <funayama.kit@gmail.com>
2020-01-19 16:26:08 +0900

    リポジトリからデータを集めるところまで終わった

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ gitのリポジトリに含まれるソースファイル（.javaのみを想定
 cregitを適用する前後のリポジトリを保存する．
 適用した後，ブランチ切ってコミットしておけばtokenの状態のファイルがmasterブランチに残る．
 
-TODO
+### やってること
 - javaファイルのパスの一覧を取得
-- beforeからLOCを計算
-- afterからif, for, while, begin_class, の数を取得
+- repo/before からLOCを計算
+- repo/after からif|if, switch|switch, for|for, while|while, do|while, try|try, begin_import, begin_class, begin_function の数を取得
+- 結果をDBにぶちこむ
--- src/collect_data.py
+++ src/collect_data.py
@@ -1,4 +1,3 @@
-import subprocess
 import glob
 
 
@@ -11,27 +10,106 @@ def get_java_file_list(repo_name):
     return java_files
 
 
-def count_LOC(files):
+def get_LOC(files):
     """
     /repo/before/[repository]/を見てLOCを出す
     :param files: fileのパスのリスト
     :return: [file_path, LOC]を要素に持つ二次元リスト
     """
     data = []
-    head = '../repo/before/'
+    head = '../repo/before'
     for file in files:
         path = head + file
+        # print(path)
         data.append([file, sum(1 for line in open(path, mode='r'))])
     # print(len(data))
     # print(data)
     return data
 
 
-# TODO: afterから色々取得する
+def find_begin_hoge(files):
+    """
+    'begin_hoge'の一覧（とか）を表示
+    """
+    head = '../repo/after'
+    begin_list = []
+    # ast_list = []
+    for i in range(len(files)):
+        path = head + files[i]
+        with open(path) as f:
+            lines = f.readlines()
+        for line in lines:
+            if 'begin_' in line:
+                if line[:-1] not in begin_list:
+                    begin_list.append(line[:-1])
+                # if line[:-1] == 'begin_decl_stmt':
+                #     print(path)
+            # elif 'name|' in line:
+            #     pass
+            # elif 'literal|' in line:
+            #     pass
+            # elif 'comment|' in line:
+            #     pass
+            # elif 'import|' in line:
+            #     pass
+            # elif 'operator|' in line:
+            #     pass
+            # else:
+            #     if line[:-1] not in ast_list:
+            #         ast_list.append(line[:-1])
+    for begin_hoge in begin_list:
+        print(begin_hoge)
+    # for ast in ast_list:
+    #     print(ast)
+
+
+def get_metrics(files):
+    """
+    'file_path'や'if'などをキーに持つ辞書のリストを作成する．
+    'if'などの出現する回数をファイルごとに数える．
+    :param files: fileのパスのリスト
+    :return: 作成した辞書リスト
+    """
+    path_loc = get_LOC(files)
+    data_list = []
+    for i in range(len(path_loc)):
+        data = {'file_path': path_loc[i][0], 'LOC': path_loc[i][1],
+                'if': 0, 'switch': 0, 'for': 0, 'while': 0, 'do-while': 0, 'try': 0,
+                'begin_import': 0, 'begin_class': 0, 'begin_function': 0}
+        data_list.append(data)
+
+    head = '../repo/after'
+    for data in data_list:
+        path = head + data['file_path']
+        with open(path) as f:
+            lines = f.readlines()
+        for line in lines:
+            if 'if|' in line:
+                data['if'] += 1
+            elif 'switch|' in line:
+                data['switch'] += 1
+            elif 'for|' in line:
+                data['for'] += 1
+            elif 'while|' in line:
+                data['while'] += 1
+            elif 'do|while' in line:
+                data['do-while'] += 1
+            elif 'try|' in line:
+                data['try'] += 1
+            elif 'begin_import' in line:
+                data['begin_import'] += 1
+            elif 'begin_class' in line:
+                data['begin_class'] += 1
+            elif 'begin_function' in line:
+                if 'begin_function_decl' in line:
+                    pass
+                else:
+                    data['begin_function'] += 1
+    return data_list
 
 
 if __name__ == '__main__':
     repository_name = 'ant'
     file_paths = get_java_file_list(repository_name)
-    # print(len(file_paths))
-    res = count_LOC(file_paths)
+    # find_begin_hoge(file_paths)
+    res = get_metrics(file_paths)
